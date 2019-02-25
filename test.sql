7f11699a6fe001dfe509e78f273ad709784ae2fa
Srijan Sinha <srijansinha159@gmail.com>
2019-02-25 22:47:08 +0530

    New tables

--- test.sql
+++ test.sql
@@ -28,3 +28,13 @@ count(IMPORTATION_OF_GIRLS_FROM_FOREIGN_COUNTRIES) as IMPORTATION_OF_GIRLS_FROM_
 count(CAUSING_DEATH_BY_NEGLIGENCE) as CAUSING_DEATH_BY_NEGLIGENCE,
 count(OTHER_IPC_CRIMES) as OTHER_IPC_CRIMES,
 count(TOTAL_IPC_CRIMES) as TOTAL_IPC_CRIMES from districtipcdata group by (STATE_OR_UT, YEAR);
+
+create view cs1160315_queryopti as select paperid from paper where upper(title) like '%QUERY%OPTIMIZATION%';
+
+select * from ls2009candi, attendancedata where UPPER(name) like '%' || UPPER (candidate_name) || '%';
+
+create trigger addnewcandidate as
+	after insert on attendancedata
+	execute procedure functio
+
+create table agriculture as select state_name as state, district_name as district, crop_year as year, season, sum(cast(production as float)) as production, sum(cast(area as float)) as area from agriproduction group by (state_name, district_name, crop_year, season) order by (state_name, district_name, year, season);
