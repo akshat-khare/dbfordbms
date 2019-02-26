517ee79eeca0e25f13e0ee9551488ae518330c4a
DivyanshuSaxena <dssaxena2011@gmail.com>
2019-02-27 02:28:35 +0530

    Added percentage attendance in mp.php

--- navbar.php
+++ navbar.php
@@ -10,12 +10,12 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="about.php">About</a>
+                    <a class="nav-link" href="about.php">Know Your District</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                    <a class="nav-link" href="contact.html">Know Your State</a>
                 </li>
-                <li class="nav-item dropdown">
+                <!-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         Blog
@@ -25,7 +25,7 @@
                         <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
                         <a class="dropdown-item" href="blog-post.html">Blog Post</a>
                     </div>
-                </li>
+                </li> -->
             </ul>
         </div>
     </div>
