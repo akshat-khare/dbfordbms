0e5e41c6bf7ce1f0c9e1291d03a8997ccde457f6
DivyanshuSaxena <dssaxena2011@gmail.com>
2019-02-26 16:38:19 +0530

    Added footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer style="margin-top: 50px;">
+    <hr>
+        <span>
+            Built by
+            <a href="https://github.com/akshat-khare" target="_blank">@akshat-khare</a>, 
+            <a href="https://github.com/srijan-sinha" target="_blank">@srijan-sinha</a> and
+            <a href="https://github.com/DivyanshuSaxena" target="_blank">@DivyanshuSaxena</a>
+            using
+            <a href="http: //php.net/manual/en/intro-whatis.php" target="_blank">PHP</a> &
+            <a href="https://code.visualstudio.com" target="_blank">VSCode</a>.
+        </span>
+
+<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
